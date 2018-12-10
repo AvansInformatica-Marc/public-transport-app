1a523a584ba0129fe4db0d14f52db5155af11901
Marc <marc.j.bouwman@gmail.com>
2018-12-10 20:36:10 +0100

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TimetableComponent } from './components/timetable/timetable.component';
+import { EditComponent } from './components/timetable/edit/edit.component';
+import { DetailsComponent } from './components/timetable/details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "timetable", component: TimetableComponent, children: [
+    { path: "new", component: EditComponent },
+    { path: ":id", component: DetailsComponent },
+    { path: ":id/edit", component: EditComponent }
+  ] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

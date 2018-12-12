860fdbd5809fe5a4084fadc9f405d036956b3c5b
Marc <marc.j.bouwman@gmail.com>
2018-12-12 13:33:53 +0100

    Improvements

--- src/app/components/timetable/edit/edit.component.ts
+++ src/app/components/timetable/edit/edit.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
-
   constructor() { }
 
-  ngOnInit() {
-  }
-
+  public ngOnInit() {}
 }
